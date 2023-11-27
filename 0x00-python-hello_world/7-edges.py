1285c44ea6a2ecec86ee03453129e875f2f89cf7
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-27 19:10:27 +0100

    used the slicing method to solve this task

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word1 = word[:4]
+word2 = word[-2:]
+word3 = word[1:9]
+print(f"First 3 letters: {word1}")
+print(f"Last 2 letters: {word2}")
+print(f"Middle word: {word3}")
