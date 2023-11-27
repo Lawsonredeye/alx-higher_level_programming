a3fc21c2ee529d9cd3a5bf36cd0b63e37893f637
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-27 21:01:12 +0100

    pycodestyle is here too

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word1 = word[:3]
 word2 = word[-2:]
-word3 = word[1: (len(word) -1)]
+word3 = word[1:(len(word) - 1)]
 print(f"First 3 letters: {word1}")
 print(f"Last 2 letters: {word2}")
 print(f"Middle word: {word3}")
