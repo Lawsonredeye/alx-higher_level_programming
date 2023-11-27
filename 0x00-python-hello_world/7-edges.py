40927232a8fe1591fd6822aef5a4b789f8950d14
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-27 19:13:43 +0100

    used the lenght to actually calculate the first index and the last

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word1 = word[:4]
 word2 = word[-2:]
-word3 = word[1:9]
+word3 = word[1: (len(word) -1)]
 print(f"First 3 letters: {word1}")
 print(f"Last 2 letters: {word2}")
 print(f"Middle word: {word3}")
