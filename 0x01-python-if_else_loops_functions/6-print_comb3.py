a115fb89fab725720e3fe5d15cb32216bf337469
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-28 19:50:37 +0100

    used the range() func and then another for loop with a range but with a check in place to see if j == 8 and i == 9 to stop the last comma

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(10):
+    # Start the inner loop from i + 1
+    for j in range(i + 1, 10):
+        if j == 9 and i == 8:
+            print("{}{}".format(i, j), end=' ')
+        else:
+            print("{}{},".format(i, j), end=' ')
+
+print()
