6973b0bd7f085cecb339771973e04b259a5b75a9
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-28 21:15:19 +0100

    function that turns lowercase str to upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            value = ord(i) - 32
+            value = chr(value)
+            new_str += value
+        else:
+            value = i
+            new_str += i
+    print(new_str)
