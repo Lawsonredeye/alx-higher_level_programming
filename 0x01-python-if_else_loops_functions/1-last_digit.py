15df360e4a3aed113926ba28ae0c716a9e2a7a7d
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-28 22:13:44 +0100

    used the modulus %

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# type cast the int
-number2 = str(number)
-# stored the sliced string and typecast it
-number3 = int(number2[-1])
-if number3 > 5:
-    print(f"Last digit of {number} is {number3} and is greater than 5")
-elif number3 == 0:
-    print(f"Last digit of {number} is {number3}")
-elif number3 < 6 and number3 != 0:
-    print(f"Last digit of {number} is {number3} and is less than 6 and not 0")
+last = number % 10
+if number < 0:
+    last = number % -10
+if last > 5:
+    print(f"Last digit of {number:d} is {last:d} and is greater than 5")
+elif last == 0:
+    print(f"Last digit of {number:d} is {last:d} and is 0")
+else:
+    print(f"Last digit of {number:d} is {last:d} and is less than 6 and not 0")
