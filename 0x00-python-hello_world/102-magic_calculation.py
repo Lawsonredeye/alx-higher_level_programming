1a538fa670f1bb9e3aa9697bbf8a797e02c525b2
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-30 00:21:58 +0100

    updated

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     return (98 + (a**b))
-
-# Import dis module 
-# Use dis.dis to get the byte code of the passed variable
-import dis
-dis.dis(magic_calculation)
