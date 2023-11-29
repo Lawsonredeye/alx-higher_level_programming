073303c32154d32fa101607e32cf1465661c4446
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-29 10:15:45 +0100

    function that prints fizz buzz for a given num

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+
+    for i in range(1, 50):
+        # print (f"i = {i}, mod:{i % 3}")
+        if i % 3 == 0:
+            print("Fizz", end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+        if i % (3 * 5) == 0:
+            print("FIZZBUZZ", end=' ')
+        else:
+            print(i, end=' ')
