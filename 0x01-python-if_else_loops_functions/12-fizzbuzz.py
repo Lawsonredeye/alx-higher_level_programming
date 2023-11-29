cf7cc0a4c31f60f4b08b3a167415ca66407baf94
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-29 10:19:20 +0100

    increased the range to be a max of 101

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
 
-    for i in range(1, 50):
+    for i in range(1, 101):
         # print (f"i = {i}, mod:{i % 3}")
         if i % 3 == 0:
             print("Fizz", end=' ')
