561f53e12f1225aa0307681d47e4b1fde0a55232
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-29 11:29:22 +0100

    changed the flow and check if i is equal to the multiple of 3 and 5

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,11 @@ def fizzbuzz():
 
     for i in range(1, 101):
         # print (f"i = {i}, mod:{i % 3}")
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
+        elif i % 3 == 0:
             print("Fizz", end=' ')
-        elif i % 5 == 0:
+        if i % 5 == 0:
             print("Buzz", end=' ')
-        if i % (3 * 5) == 0:
-            print("FIZZBUZZ", end=' ')
         else:
             print(i, end=' ')
