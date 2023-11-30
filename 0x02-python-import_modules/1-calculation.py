7ac386996a3b3d0eafe32b1fb99c60f4b28bd5b1
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-30 21:17:50 +0100

    used the .format() for printing out strings

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print(f"{a} + {b} = {add(a, b)}")
-print(f"{a} - {b} = {sub(a, b)}")
-print(f"{a} * {b} = {mul(a, b)}")
-print(f"{a} / {b} = {div(a, b)}")
+print("{a} + {b} = {add(a, b)}".format(a, b, add(a, b)))
+print("{a} - {b} = {sub(a, b)}".format(a, b, sub(a, b)))
+print("{a} * {b} = {mul(a, b)}".format(a, b, mul(a, b)))
+print("{a} / {b} = {div(a, b)}".format(a, b, div(a, b)))
