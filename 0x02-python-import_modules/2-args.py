f3993826a8a67a0e8ad5659beab09d56a984c4e7
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-30 22:30:13 +0100

    added the if __name__ == "__main__"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-argv = sys.argv[1:]
-if len(argv) == 0:
-    print("0 arguments.")
-elif len(argv) == 1:
-    print("1 argument:")
-    for i, argument in enumerate(argv, start=1):
-        print("{}: {}".format(i, argument))
-else:
-    print("{} arguments:".format(len(argv)))
-    for i, argument in enumerate(argv, start=1):
-        print("{}: {}".format(i, argument))
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    if len(argv) == 0:
+        print("0 arguments.")
+    elif len(argv) == 1:
+        print("1 argument:")
+        for i, argument in enumerate(argv, start=1):
+            print("{}: {}".format(i, argument))
+    else:
+        print("{} arguments:".format(len(argv)))
+        for i, argument in enumerate(argv, start=1):
+            print("{}: {}".format(i, argument))
