c95de80402fb1d6a664ee026934c61f19a648c14
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-03 19:05:26 +0100

    removed the for loop and just returned the index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list) - 1:
         return None
     else:
-        for i in my_list[idx]:
-            if i == idx:
-                return (my_list[idx])
+        return (my_list[idx])
