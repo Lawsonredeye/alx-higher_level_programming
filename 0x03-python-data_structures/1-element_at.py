b8cd3623acb6dbdc6f3513f2ef2bd462b97df462
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-03 18:57:13 +0100

    removed the if __name__ at the top

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
-        if idx < 0:
-            return None
-        elif idx > len(my_list):
-            return None
-        else:
-            for i in my_list:
-                if i == idx:
-                    return (my_list[i])
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        for i in my_list:
+            if i == idx:
+                return (my_list[i])
