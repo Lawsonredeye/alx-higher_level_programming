5609a3b85f8d987ec567f9c6bc72e82841338e2d
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-03 23:17:11 +0100

    removed c

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0:
-        return (c)
+        return new_list
     elif idx >= len(my_list):
         return new_list
     else:
