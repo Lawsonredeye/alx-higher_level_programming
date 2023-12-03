534a8d238f7c387919814716d39393ee5126f162
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-03 19:21:30 +0100

    added the = sign

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,6 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return (my_list)
     else:
-        my_list = new_list
+        my_list[idx] = new_list
         new_list[idx] = element
         return (new_list[idx])
