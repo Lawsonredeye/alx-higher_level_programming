6614d32ac535badad9d34a060d50a3c0caec890f
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-05 15:54:12 +0100

    made changes to the file by using 'if not matrix' also used the .join() method

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,13 +4,3 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for i in range(len(matrix)):
         print(" ".join("{:d}".format(matrix[i][j]) for j in range(len(matrix[i]))))
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
