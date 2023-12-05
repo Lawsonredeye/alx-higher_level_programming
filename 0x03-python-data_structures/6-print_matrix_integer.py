98707073ac9db525afbb43a6127985b9bb7e8d78
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-05 19:50:18 +0100

    updated it

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,13 +7,3 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             row_as_str.append(str(matrix[i][j]))
         print(" ".join(row_as_str))
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
