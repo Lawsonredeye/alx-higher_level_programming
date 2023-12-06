f032302fa0c6396c406e802c0630196d05e32921
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-06 21:41:42 +0100

    returned my_list if empty

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if not my_list:
-        return None
+        return my_list
     new_list = my_list.copy()
     for i in range(len(new_list)):
         if new_list[i] == search:
