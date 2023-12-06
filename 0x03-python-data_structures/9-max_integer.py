cba5f0bb82be85c16e3bbe55b6a485900207a1d9
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-06 10:32:29 +0100

    fixed indentation

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    x = my_list[0]
     if not my_list:
         return None
+    x = my_list[0]
     for i in my_list:
         if i > x:
             x = i
