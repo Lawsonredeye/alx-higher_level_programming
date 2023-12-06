6e3c254f43d57101462c402c3531b5592cf97586
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-06 22:52:50 +0100

    used .keys() func

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     total = 0
-    for i in range(len(a_dictionary)):
+    for i in range(len(a_dictionary.keys())):
         total += i
     return total
