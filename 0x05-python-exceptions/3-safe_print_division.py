949bfc533bbddd95e40c1649788cba9f42f4fb4b
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-18 23:14:17 +0100

    fixed pycodestyle

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,14 +7,4 @@ def safe_print_division(a, b):
         result = None
     finally:
         print("Inside result: {}".format(result))
-    return result
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+        return result
