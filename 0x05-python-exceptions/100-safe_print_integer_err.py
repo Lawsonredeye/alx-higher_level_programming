211d1b63dab019438af9e132843c2a0865727f9b
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-19 12:48:30 +0100

    added TypeError

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as value:
+    except (ValueError, TypeError) as value:
         print("Exception: {}".format(value))
         return False
