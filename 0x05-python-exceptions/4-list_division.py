5834934cf4ebf1fc7c8570321499dba5cb928fba
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-19 08:32:16 +0100

    used pass for the finally clause

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,4 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             lenght.append(0)
         finally:
-            return lenght
+            pass
+    return lenght
