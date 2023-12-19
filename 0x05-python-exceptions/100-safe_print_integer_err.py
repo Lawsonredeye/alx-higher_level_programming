cf9688583d1bd02ef1bfffde42bd880274a83d66
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-19 12:04:04 +0100

    used the .format() string

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,5 +4,20 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as value:
-        print("Exception:", value)
+        print("Exception: {}".format(value))
         return False
+
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
