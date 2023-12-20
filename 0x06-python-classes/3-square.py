0fb72d521e07714a4146769c2ae17f89c0deb063
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-20 13:39:09 +0100

    clas with the area method  that returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+class Square that defines a square by: (based on 1-square.py)
+"""
+
+
+class Square:
+    """
+    class that just pass when called and accessed
+    Attributes:
+        size
+    """
+
+    def __init__(self, size=0):
+        """
+        using the __init__ method to initialize the code
+        Attributes:
+            size: just the size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """
+        using the __init__ method to initialize the code
+        Attributes:
+            size: just the size of the square
+        """
+
+        return self.__size * self.__size
