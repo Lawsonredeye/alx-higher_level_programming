1b663ed4384b591d1cf94e4dff7756176c09c283
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-20 01:33:24 +0100

    specified no attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """class Square that defines a square by: (based on 0-square.py)
+
 """
 class Square:
     """class that just pass when called and accessed
+    Attributes:
+        No Attributes
     """
     def __init__(self, size):
         """using the __init__ method to initialize the code
