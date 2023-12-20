e6615c116240e0c038130b70f63a95cd7eb40947
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-20 02:00:06 +0100

    google doc style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""class Square that defines a square by: (based on 0-square.py)
 """
-
+class Square that defines a square by: (based on 0-square.py)
+"""
 
 class Square:
     """class that just pass when called and accessed
         Attributes:
-        No Attributes
+            size
     """
 
     def __init__(self, size):
-        """using the __init__ method to initialize the code
-        
-        Args:
-            size (int): just the size of the square on the private mode
         """
-
+        using the __init__ method to initialize the code
+        
+        Attribute:
+            size
+        """
         self.__size = size
