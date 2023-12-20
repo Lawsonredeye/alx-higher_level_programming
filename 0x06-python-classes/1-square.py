72e513e82aca1a6069667f7a6e7c469a9efdf275
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-20 01:42:31 +0100

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """class Square that defines a square by: (based on 0-square.py)
-
 """
+
+
 class Square:
     """class that just pass when called and accessed
-    Attributes:
+        Attributes:
         No Attributes
     """
     def __init__(self, size):
