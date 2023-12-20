515f41179c1764e59fdf7aae289e7cde176abdb5
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-20 13:06:13 +0100

    used the if statement, inistance and raise

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""
+class Square that defines a square by: (based on 1-square.py)
+"""
+
 class Square:
+    """
+    class that just pass when called and accessed
+    Attributes:
+        size
+    """
+
     def __init__(self, size=0):
-        try:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            if isinstance(size, int):
-                raise TypeError("size must be an integer")
-        except (TypeError):
-            print("size must be an integer")
-        self.__size = size
-        
+        """
+        using the __init__ method to initialize the code
+        Attributes:
+            size: just the size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
+     
