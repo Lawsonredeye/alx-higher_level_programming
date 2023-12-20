e87e6cdba07bd4e4625caa61778ef8c13927ada9
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-20 01:13:38 +0100

    square class that defines the 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)
+"""
+class Square:
+    """class that just pass when called and accessed
+    """
+    def __init__(self, size):
+        """using the __init__ method to initialize the code
+        
+        Args:
+            param1 (size): just the size of the square on the private mode
+        """
+        self.__size = size
