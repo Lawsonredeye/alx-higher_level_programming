f967e452853117e67a8d08254d629be8bd35c286
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-06 23:16:11 +0100

    created a readme doc and an emppty class module

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+
+    """Function that s just empty
+
+    Args:
+        no argument
+    """
+
+    pass
