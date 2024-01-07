0f1a8d8e7738537cd514e885b27d33b86fcf90cc
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-08 00:51:38 +0100

    implemented a function that prints a newline when it encounters ., ? and :

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-
+"""
+Module containing a function that prints to the stdout
+Name: matrix_divided
+"""
 def matrix_divided(matrix, div):
+    """
+    function that divides all elements of a matrix.
+
+    Args:
+        matrix (list): takes in a list as an argument
+        div (int): divisor must be an integer 
+    """
     pass
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a function that breaks a string based on the input
+if it encounters a '.', '?' and ':' it prints a newline
+"""
+
+
+def text_indentation(text):
+    """
+    function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+
+    Args:
+        str: takes in text as a string
+
+    Raises:
+        TypeError: if the parameter passed is not a string
+    """
+
+    if not isinstance(text , str):
+        raise TypeError("text must be a string")
+    for i in text:
+        if i == "." or i == "?" or i == ":":
+            print("\n")
+        else:
+            print(i, end="")
