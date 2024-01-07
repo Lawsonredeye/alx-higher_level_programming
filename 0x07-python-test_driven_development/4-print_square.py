6271e3a89afc872a9db69759daf456bb4db5c0d9
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-08 00:14:59 +0100

    implemented a module and a test file for printing '#' based on the size value passed

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module which has a printing function
+Does not return anything
+"""
+
+
+def print_square(size):
+    """
+    function that prints a square with the character #
+    Args:
+        size: must be an integer to specified the size
+    
+    Raises:
+        TypeError: raises an exception
+        ValueError: raises an exception
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    else:
+        for i in range(size):
+            print("#"*size)
