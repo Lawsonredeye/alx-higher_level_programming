3cbf7c3717a7ef55ea659cd1bb812b3cc8994cc7
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-08 01:43:49 +0100

    created a file for unittesting =, still under development

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+"""
+Module that carries out unittesting
+"""
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    unittest to check if a specific list operation is successfully carried out
+    """
+    def test_max_integer_with_positive_numbers(self):
+        self.assertEqual()
