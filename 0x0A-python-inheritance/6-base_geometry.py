a660fdc1d52e6a42c00fd58546aef65c255f8f97
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-10 00:19:31 +0100

    functions and classes

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """
+    Class for BaseGeometry that has several functionalities
+    """
+
+    def area(self):
+        """
+        that raises an Exception with the 
+        message area() is not implemented
+
+        Raises:
+            Exception
+        """
+
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 6-base_geometry.py).
+Public instance method: def area(self): that raises an Exception with the message area() is not implemented
+Public instance method: def integer_validator(self, name, value): that validates value:
+you can assume name is always a string
+if value is not an integer: raise a TypeError exception, with the message <name> must be an integer
+if value is less or equal to 0: raise a ValueError exception with the message <name> must be greater than 0
+You are not allowed to import any module
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        that raises an Exception with the 
+        message area() is not implemented
+
+        Raises:
+            Exception
+        """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        that validates value:
+        you can assume name is always a string
+        if value is not an integer: raise a TypeError exception,
+        with the message <name> must be an integer
+        if value is less or equal to 0: raise a ValueError 
+        exception with the message <name> must be greater than 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
