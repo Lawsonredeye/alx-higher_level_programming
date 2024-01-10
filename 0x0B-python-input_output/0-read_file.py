88707b5701d47b37c73971fed411ca5de3146053
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-10 21:30:41 +0100

    Readme, function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ This module contains a function that reads a file
 
 def read_file(filename=""):
     """
-    unction that reads a text file (UTF8)
+    function that reads a text file (UTF8)
     and prints it to stdout using the print function
 
     Args:
