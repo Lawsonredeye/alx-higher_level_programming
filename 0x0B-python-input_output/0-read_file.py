62c9d67e244d7830e4f2a86cc1699a759d7cde1d
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-10 21:30:27 +0100

    Readme, function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains a function that reads a file
+"""
+
+
+def read_file(filename=""):
+    """
+    unction that reads a text file (UTF8)
+    and prints it to stdout using the print function
+
+    Args:
+        filename (str): a string which is a path to a file
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+# 0x0B. Python - Input/Output
+open() returns a file object, and is most commonly used with two positional arguments and one keyword argument: open(filename, mode, encoding=None)
