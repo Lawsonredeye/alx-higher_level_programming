b1a9a27a34e469972c27b63d8db5b475f727b6cc
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-11 02:23:55 +0100

    python script that add arguments and store inside a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that creates an
+Script that adds all arguments to a Python list,
+and then save them to a file using json and files
 """
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
