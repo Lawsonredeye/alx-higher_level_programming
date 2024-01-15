461b3b95309cebb4ba7a0d910545a3a99a3a2704
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-15 10:41:47 +0100

    test cases and test folders for base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,5 +13,25 @@ class TestBase(unittest.TestCase):
     if it actually works as expected and if it raises any errors
     """
 
+    def setUp(self):
+        """
+        initializes setUp calls which is excecuted before every
+        process is executed
+        """
+        self.a = Base()
+        self.b = Base()
+
     def test_isTrue(self):
+        """
+        checks if the expression is actually True
+        """
         self.assertTrue(True)
+    
+    def test_isEquals(self):
+        """
+        checks if the self.a.id is equal to the output
+        being passed using the setUp() method to initialize
+        the test_isEquals method
+        """
+        self.assertEqual(self.a.id, 1)
+        self.assertEqual(self.b.id, 2)
