6898bcff0042e60c7e9b344cccafa59755eb89ed
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-15 20:12:48 +0100

    added edge cases where all inputs must be an int else it would raise a ValueError

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,10 +33,28 @@ class Rectangle(Base):
             y (int): 0 (Default) optional if not passed
             id (int): None (Default)
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif width < 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else: self.__x = x
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else: self.__y = y
         super().__init__(id)
 
     @property
