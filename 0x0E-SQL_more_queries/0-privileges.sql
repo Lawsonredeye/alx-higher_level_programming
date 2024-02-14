c7c202f57b45e2aad484bec8d4822880ebe13fb0
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-14 22:04:53 +0100

    added the @host to it.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 --  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW GRANT FOR user_0d_1, user_0d_2;
+SHOW GRANT FOR user_0d_1@localhost, user_0d_2@localhost;
