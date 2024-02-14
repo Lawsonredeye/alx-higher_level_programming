17d8344b29d6d40a02cbbacc94d0f98dd2fa586f
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-14 23:42:48 +0100

    changed the name of the table

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- creates the table id_not_null on your MySQL server.
-CREATE TABLE IF NOT EXISTS id_not_null(
+CREATE TABLE IF NOT EXISTS unique_id(
 	id INT DEFAULT 1 UNIQUE,
 	name VARCHAR(256)
 	);
