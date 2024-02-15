ab2af35627cf0b915a658990ce838f54d5151c52
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-15 16:07:18 +0100

    updated

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,5 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
 SELECT score, name
 	FROM second_table
-	GROUP BY score, name
-	ORDER BY score DESC
-	WHERE name IS NOT NULL;
+	WHERE name IS NOT NULL
+	ORDER BY score DESC;
