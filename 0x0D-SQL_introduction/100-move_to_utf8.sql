c9fe244e207eedf23e0ca6521e603968958773c5
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-16 00:44:46 +0100

    up

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,8 @@
--- converts database to utf-8
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET 
-utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Script to convert hbtn_0c_0 database to UTF8MB4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
+-- Script to convert first_table table to UTF8MB4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
-	MODIFY COLUMN name VARCHAR(255)
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
+-- Script to convert name field in first_table to UTF8MB4
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
