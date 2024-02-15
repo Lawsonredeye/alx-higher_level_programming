9a49ae5ae02c41b4390395c4fce88f6603a3d8d6
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-15 16:05:43 +0100

    modified

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
-SELECT *
-	FROM hbtn_0c_0
+SELECT score, name
+	FROM second_table
 	GROUP BY score, name
 	ORDER BY score DESC
-	WHERE name NOT NULL;
+	WHERE name IS NOT NULL;
