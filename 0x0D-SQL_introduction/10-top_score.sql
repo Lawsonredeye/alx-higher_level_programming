eb4244607f1e0ac16dccbea3a41669b40313e97d
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-15 09:22:55 +0100

    used DESC

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- list scores of the table by name and score in ascending order
 
-SELECT score, name FROM second_table ORDER BY score ASC;
+SELECT score, name FROM second_table ORDER BY score DESC;
