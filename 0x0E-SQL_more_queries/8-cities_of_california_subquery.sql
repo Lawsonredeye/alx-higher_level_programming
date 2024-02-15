093148f01c63f3c96f56db7c24015e743131f22d
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-15 01:05:58 +0100

    used states

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 --  script that lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT name,
-FROM STATES,
+FROM states,
 WHERE name = "California",
 ORDER BY id ASC;
