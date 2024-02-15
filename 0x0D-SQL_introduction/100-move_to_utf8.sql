e0ff6de246470c7fd8fac6f29eb307045ecc868d
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-15 23:45:16 +0100

    used TABLE

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- converts database to utf-8
-
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- Prints the full description of a table in an MySQL server
-SHOW CREATE DATABASE first_table;
+SHOW CREATE TABLE first_table;
