91032874f9cc3452b287716bdead5e56a44280df
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-15 09:21:41 +0100

    used a one-liner

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,3 @@
 -- list scores of the table by name and score in ascending order
-USE hbtn_0c_0
-SELECT score, name 
-FROM second_table
-ORDER BY score ASC;
+
+SELECT score, name FROM second_table ORDER BY score ASC;
