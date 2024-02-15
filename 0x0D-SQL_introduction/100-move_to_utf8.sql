42b24d2fef52442a358157149c1cd11f9b463158
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-16 00:22:01 +0100

    modified the charset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,11 @@
 -- converts database to utf-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET 
+utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+	CHARACTER SET charset
+	COLLATE utf8_unicode_ci;
+ALTER TABLE first_table
+	MODIFY name VARCHAR(255)
+	CHARACTER SET utf8mb4
+	COLLATE utf8_unicode_ci;
