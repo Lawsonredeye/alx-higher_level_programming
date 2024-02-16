2475856c12ab306db0b765cc0cba3d19941b59e9
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-16 01:28:11 +0100

    added update

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --  script that lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT c.id, c.name, s.name
-FROM cities
+FROM cities c, states s
 WHERE c.state_id = s.id
 ORDER BY c.id ASC;
