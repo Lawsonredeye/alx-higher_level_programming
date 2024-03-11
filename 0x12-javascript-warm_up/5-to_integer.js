b6dc19e8f5c6c9e111fdb1d0fce9ce76158cecc3
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-12 00:39:17 +0100

    typecasted both conditions

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv[2] == Number(process.argv[2])) {
+if (Number(process.argv[2]) === Number(process.argv[2])) {
   console.log('My number: ' + process.argv[2]);
 } else {
   console.log('Not a number');
