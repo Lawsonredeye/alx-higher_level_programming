32d43703537008d0363a439273502f4736b3386c
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-12 00:27:27 +0100

    removed comments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-/**
- * create a const var named process and assign the value require ('process')
- * using console.log, concat process.argv[2] + process.argv[3]
- */
-
 const process = require('process');
 console.log(process.argv[2] + ' is ' + process.argv[3]);
