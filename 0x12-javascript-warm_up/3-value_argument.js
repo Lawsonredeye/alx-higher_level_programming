e10d2f38af4b0152b5c5f211ad10a0e226e03c43
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-12 00:13:52 +0100

    used the indexing to solve this

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
 /**
- * create a const variable named process and
+ * create a let variable named process and assign the value = require('process')
+ * using a conditional, check, if the process.argv[2] === undefined then print no argument else print process.argv[2] member
  */
+
+const process = require('process');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
