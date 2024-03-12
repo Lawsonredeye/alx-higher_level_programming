7e5878dc51cdb1421dfd6405d1d9447a438049b1
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-12 13:21:13 +0100

    function that add 2 command line arguments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/**
+ * create a const var to store the args list
+ * check if both the argv[2] and argv[3] are both numbers if they are, then add them together and print the output
+ */
+const process = require('process');
+function add (a, b) {
+  a = Number(a);
+  b = Number(b);
+  console.log(a + b);
+}
+add(process.argv[2], process.argv[3]);
