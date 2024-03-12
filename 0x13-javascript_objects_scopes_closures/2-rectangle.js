4d4ef1045e6d6c658a3ec4094032fc6900256989
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-12 23:42:25 +0100

    check if w and h are greater than 0 then nstatiate this.width and this.height = w & h respectively

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// create a class named rectangle
+// add a constructor with the parameter w & h
+// check if w and h are greater than 0 then
+// instatiate this.width and this.height = w & h respectively
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
