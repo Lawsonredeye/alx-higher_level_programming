640174bf5d5811d5c9eff3e1c468d25d1cfa43cf
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-13 00:23:10 +0100

    prints out the shape of the rectangle using a for loop and then using the repeat method on the string to get the length of the rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+// create a class named rectangle
+// add a constructor with the parameter w & h
+// check if w and h are greater than 0 then
+// instatiate this.width and this.height = w & h respectively
+// create a method print() with no param that prints the w * h of a rectangle
+// inside the method using a loop
+// create a const shape = 'X'
+// using a loop with the length of the loop as height
+// repeat the string by the size of the width and log the shape var.
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const shape = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(shape.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
