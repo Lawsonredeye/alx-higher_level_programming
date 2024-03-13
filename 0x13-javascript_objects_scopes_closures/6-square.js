7edb13d368117e9a8bd44025760cc96e742c7f71
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-13 02:03:10 +0100

    comment out the constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const Square1 = require('./5-square.js');
 class Square extends Square1 {
-  constructor (size) {
-    super(size, size);
-  }
+//   constructor (size) {
+//     super(size, size);
+//   }
 
   charPrint (c) {
     if (c === 'C') {
