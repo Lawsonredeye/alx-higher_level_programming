9e753c7bdeaaa5fe719a8b8be45c8a011188fa19
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-13 02:37:13 +0100

    implemented a function that reverses a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// create a emptyList = []
+// access the last element of the list using list.lenght - 1 to the loop counter and decrement the value into a new list and return the emptyList
+exports.esrever = function (list) {
+  const emptyList = [];
+  for (let i = list.length - 1; i > 0; i--) {
+    emptyList.push(list[i]);
+  }
+  return emptyList;
+};
