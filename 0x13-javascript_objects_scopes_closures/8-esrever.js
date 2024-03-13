7a95d2f8c3124be3d1700d8f91a29f200124d363
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-13 22:30:42 +0100

    changed variable name

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// create a emptyList = []
-// access the last element of the list using list.lenght - 1 to the loop counter and decrement the value into a new list and return the emptyList
+// create a reverseList = []
+// access the last element of the list using list.lenght - 1 to the loop counter and decrement the value into a new list and return the reverseList
 exports.esrever = function (list) {
-  const emptyList = [];
+  const reverseList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    emptyList.push(list[i]);
+    reverseList.push(list[i]);
   }
-  return emptyList;
+  return reverseList;
 };
