102061c97184180004fd7d7e87d0c674f022dec5
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-13 03:14:04 +0100

    used a normal function to just implement a function that prints the number of arguments already printed and the new argument value.

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// use a closure function, return the new function
+// innerfunc should have a count variable
+// if new argument is passed increment count
+let count = 0;
+exports.logMe = function (item) {
+  console.log(`${count}: ${item}`);
+  count += 1;
+//   return function newLog(item) {
+//   };
+};
