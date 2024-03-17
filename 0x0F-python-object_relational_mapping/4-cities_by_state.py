c30554c2c5267e5645f4afb89c9d87fe223da623
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-17 20:30:01 +0100

    update

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+def main():
+    '''
+    This handles the execution of the script if the main is excuted
+    It takes no argument as a parameter and also use the command line argument
+    for working with data.
+    '''
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    # create a cursor
+    db_cursor = db.cursor()
+    # execute the cursor
+    db_cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    query_rows = db_cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    # close the connection for the cursor and database
+    db_cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    '''
+    This checks if the script is
+    running as main or also if its imported as a module into another module
+    '''
+    main()
