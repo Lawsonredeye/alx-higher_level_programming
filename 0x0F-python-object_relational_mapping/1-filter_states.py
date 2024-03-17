bd604178089ff53db38d696ccdfa2d3202ef51b4
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-17 02:29:34 +0100

    changes database to db

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -32,7 +32,7 @@ def main():
     db_connect = MySQLdb.connect(host='localhost',
                                  port=3306, user=sys.argv[1],
                                  passwd=sys.argv[2],
-                                 database=sys.argv[3])
+                                 db=sys.argv[3])
     db_cursor = db_connect.cursor()
     db_cursor.execute(
         "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
