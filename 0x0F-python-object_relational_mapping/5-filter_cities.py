d6583bcb847bd473f633d6674d9681b4b8c45eb2
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-18 09:34:06 +0100

    fixed pycodesytle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Write a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
+Write a script that takes in the name of a state as an
+argument and lists all cities of that state, using the database hbtn_0e_4_usa
 
-Your script should take 4 arguments: mysql username, mysql password, database name and state name (SQL injection free!)
+Your script should take 4 arguments: mysql username,
+mysql password, database name and state name (SQL injection free!)
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by cities.id
@@ -39,6 +41,7 @@ def main():
     for row in query_rows:
         print(row)
     data.close()
+
 
 if __name__ == "__main__":
     main()
