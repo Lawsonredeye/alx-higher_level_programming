a4cd3eb8900629c46c6e86bd824abcfc63cbfaf4
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-18 08:12:12 +0100

    added documentation

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+
+'''
+Write a script that lists all cities from the database hbtn_0e_4_usa
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by cities.id
+You can use only execute() once
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+'''
 import sys
 import MySQLdb
 
