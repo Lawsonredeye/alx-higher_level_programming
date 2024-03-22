26229a1e735fb8ae566c1892f90d433a002e8d07
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-23 00:35:39 +0100

    update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that connects to a database and prints its values
+"""
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.id == 2).first()
+    if query is not None:
+        query.name = "New Mexico"
+        session.commit()
