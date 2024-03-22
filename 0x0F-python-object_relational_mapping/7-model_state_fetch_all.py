3b01242a24dca36fc24cc39e9c2a81d68edc722e
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-22 23:21:34 +0100

    sqlalchemy script

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-
-# import the sqlalchemy module
-# import the state and base
-# import select from sqlalchemy
-from sqlalchemy import MetaData, create_engine, Table, Column, Integer, String
-import sys
-from model_state import Base, State
+"""
+Script that connects toa database and prints its values
+"""
 
 
-engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).order_by(State.id)
+    for q in query:
+        print(f'{q.id}: {q.name}')
