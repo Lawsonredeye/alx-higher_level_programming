e370e61be14b30e21db273f9964d70f58db0adab
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-23 00:12:30 +0100

    updated script

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that connects to a database and prints its values
+"""
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter_by(name=sys.argv[4],)
+    if query is not None:
+        print(f'{query.id}')
+    else:
+        print('Not found')
