333ae81fbc60d87b88010ab11bf6a32914ee9e70
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-22 23:53:35 +0100

    state filter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that connects to a database and prints its values
+"""
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    for q in query:
+        print(f'{q.id}: {q.name}')
