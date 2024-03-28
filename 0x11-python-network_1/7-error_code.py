08366b03b38512e4635b0d3bdfb14bc57341518e
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-29 00:13:17 +0100

    nb

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
         print("Error code: ", r.status_code)
-    print(r.text)
+    else:
+        print(r.text)
