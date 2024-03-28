be2d55dbff76aa8bc1779c28b6a24f4fa0b9091f
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-28 23:04:56 +0100

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found
+in the header of the response.
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+data = sys.argv[2]
+data = data.encode()
+req = urllib.request.Request(sys.argv[1], data=data, method='POST')
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode()
+    print(body)
