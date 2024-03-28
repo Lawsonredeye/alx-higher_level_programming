f5ca6b4d06c717045fc289f1be5876b72c986c77
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-29 00:11:11 +0100

    added space

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
-        print("Error code:", r.status_code)
+        print("Error code: ", r.status_code)
     print(r.text)
