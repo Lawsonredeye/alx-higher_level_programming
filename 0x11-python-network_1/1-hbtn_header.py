81734a80d70b245c67d5d14b1097a62a842c80d7
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-28 22:35:49 +0100

    script that gets the X-id from a url passed on the command line

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found
+in the header of the response.
+"""
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    body = response.info()
+    print(body['X-Request-Id'])
