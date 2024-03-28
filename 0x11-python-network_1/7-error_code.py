407929c9b57254ee36afebaf6b702106e81371ee
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-29 00:08:40 +0100

    took care of when an error code is encountered

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script which uses request library instead of urllib"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    print(r.text)
