75df0c950d20f193e4de138af73a70c89132f2aa
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-28 15:22:02 +0100

    push

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # This script sends a get request and also and displays the body of the response
-response=$(curl -sI $1)
-if [[ "$response" == *"200 OK"* ]]
-then
-    curl -s $1
-fi
+response=$(curl -sI $1) && [[ "$response" == *"200 OK"* ]] && curl -s $1
