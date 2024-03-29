225d61ffd9d558b1a070a5d7b868c749b560dd1a
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-29 01:36:52 +0100

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print("Error code: ", r.status_code)
-    else:
+    if r.status_code == 200:
         print(r.text)
+    elif r.status_code >= 400:
+        print("Error code: ", r.status_code)
