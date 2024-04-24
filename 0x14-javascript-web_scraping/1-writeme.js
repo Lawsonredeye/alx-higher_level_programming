c72ec342a860f60f584a3264598fe6c630910744
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-24 19:02:22 +0100

    script that writes to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/**
+ * script that writes a string to a file.
+ * import fs and process using argv
+ * store the 2 & 3 args into 2 var
+ * using fs.writeFile place the path and the data
+ * using the callback func print any err that was encountered
+ */
+
+const fs = require('fs')
+const arg2 = process.argv[2]
+const arg3 = process.argv[3]
+
+fs.writeFile(arg2, arg3, {option : "utf8"}, (err, data) => {
+  if (err) {
+    console.err(err)
+  }
+});
