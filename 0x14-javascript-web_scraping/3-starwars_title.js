c941ceab5227a2dd8e64e8f29b2b884da0e41849
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-24 20:54:04 +0100

    file that fetches json response

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const arg = process.argv[2]
+
+request(`https://swapi-api.alx-tools.com/api/films/${arg}`, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(JSON.parse(body).title)
+});
