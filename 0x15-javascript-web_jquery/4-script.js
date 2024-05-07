ad23e52015ebc32cfb57725db659eeb144e10961
lawsonredeye <omoregbeeolawson@gmail.com>
2024-05-07 01:23:00 +0100

    script that updates the text of the <header> element to New Headergit add .! when the user clicks on DIV#update_header

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$("#toggle_header").click(function(){
+  if ($("header").hasClass("green")){
+    $("header").removeClass("green");
+    $("header").addClass("red");
+  }
+  else if ($("header").hasClass("red")){
+    $("header").removeClass("red");
+    $("header").addClass("green");
+  }
+});
